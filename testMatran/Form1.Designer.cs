b3c43450634edbe8b8724a415dae6269871c8e7d
LeThang34 <104156515+LeThang34@users.noreply.github.com>
2022-06-04 23:26:55 +0700

    first commit

---
+++ testMatran/Form1.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace testMatran
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.panel1.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.panel3);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(800, 50);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.textBox1);
+            this.panel3.Controls.Add(this.label1);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel3.Location = new System.Drawing.Point(0, 0);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(577, 50);
+            this.panel3.TabIndex = 5;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Location = new System.Drawing.Point(111, 6);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(463, 22);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(-3, 3);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // button1
+            // 
+            this.button1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.button1.Location = new System.Drawing.Point(580, 0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(220, 50);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.flowLayoutPanel1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(0, 50);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(800, 400);
+            this.panel2.TabIndex = 1;
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flowLayoutPanel1.Font = new System.Drawing.Font("Webdings", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
+            this.flowLayoutPanel1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(800, 400);
+            this.flowLayoutPanel1.TabIndex = 0;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ testMatran/Form1.cs
@@ -0,0 +1,212 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace testMatran
+{
+    public partial class Form1 : Form
+    {
+        Random random = new Random();
+        List<string> icons = new List<string>()
+        {
+            //Mức độ trung bình
+             "!", "!", "N", "N", ",", ",", "k", "k", "b",
+            "b", "v", "v", "w", "w", "z", "z", "@", "@", "#", "#", "$", "$", "%", "%",
+            "^", "^", "&", "&", "*", "*", "u", "u", "a", "a", "r", "r", "q", "q", "e", "e"
+        };
+        Button buttonClicked, buttonClicked2;
+        int n;
+        int[] arr1;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            flowLayoutPanel1.Controls.Clear();
+            n = int.Parse(textBox1.Text);
+
+            arr1 = new int[n];
+
+            Random rd1 = new Random();
+            int a = 0;
+            while( a < n)
+            {
+                int x  = rd1.Next(1, n+1);   
+                if(chuaXuatHienX(arr1, a, x))
+                {
+                    arr1[a++] = x;
+                }
+            }
+
+
+            for (int i =0; i < n; i++)
+            {
+                flowLayoutPanel1.Controls.Add(btn(i));
+            }    
+        }
+
+        private static bool chuaXuatHienX(int[] arr1, int size, int x)
+        {
+            for (int i = 0; i < size; i++)
+            {
+                if (arr1[i] == x)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+
+
+        private void btn_Click(object sender, EventArgs e)
+        {
+            if (buttonClicked != null && buttonClicked2 != null)
+                return;
+
+            Button clickerLabel = sender as Button;
+
+            if (clickerLabel == null)
+                return;
+            if (clickerLabel.ForeColor == Color.Black)
+                return;
+            if (buttonClicked == null)
+            {
+                buttonClicked = clickerLabel;
+                buttonClicked.ForeColor = Color.Black;
+                return;
+            }
+            buttonClicked2 = clickerLabel;
+            buttonClicked2.ForeColor = Color.Black;
+
+
+            if (buttonClicked.Text == buttonClicked2.Text)
+            {
+                buttonClicked = null;
+                buttonClicked2 = null;
+            }
+            else
+                timer1.Start();
+
+        }
+        
+        
+        Button btn(int i)
+        {
+            
+            List<string> list = new List<string>();
+            Button b = new Button();
+            b.Name = Numrd.ToString();
+            b.Width = 62;
+            b.Height = 62;
+            b.Text = icons[i];
+
+            b.Click +=  new System.EventHandler(this.btn_Click);
+            return b;
+        }
+
+
+
+        //Game play
+
+        private void CheckForWiner()
+        {
+            for (int i = 0; i < flowLayoutPanel1.Controls.Count; i++)
+            {
+                Label label;
+                label = flowLayoutPanel1.Controls[i] as Label;
+                if (label != null && label.ForeColor == label.BackColor)
+                    return;
+            }
+
+            MessageBox.Show("You win!", "Thông báo");
+            // Close();
+        }
+
+
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            timer1.Stop();
+
+            buttonClicked.ForeColor = buttonClicked.BackColor;
+            buttonClicked2.ForeColor = buttonClicked2.BackColor;
+
+            buttonClicked = null;
+            buttonClicked2 = null;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            if(textBox1.Text == "")
+            {
+                return;
+            }
+            else
+            {
+
+            int tbValue = Convert.ToInt32(textBox1.Text);
+            
+            if (tbValue % 2 != 0)
+            {
+                MessageBox.Show("Số ô là lẻ không thể chơi (Cần nhập số chẵn)", "Hệ thống",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error
+                    );
+                return;
+            }
+            if (tbValue > 40 )
+            {
+                MessageBox.Show("Vượt quá giới hạn 40 ô", "Hệ thống", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            if (tbValue == 0)
+                {
+                    MessageBox.Show("Số phần tử phải nguyên dương", "Hệ thống", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            
+        }
+
+        //Hàm gán biển tượng cho các ô vuông
+        /*
+        private void AssignIconsToSquares()
+        {
+            //Tạo biến ngẫu nhiên để lưu trữ tạm thời
+            Button label;
+            //Biến chứa số ngẫu nhiên để gán biểu tượng vào ô có số thứ tự đó
+            int randomNumber;
+
+            for (int i = 0; i < n; i++)
+            {
+                if (flowLayoutPanel1.Controls[i] is Button)
+                {
+                    label = (Button)flowLayoutPanel1.Controls[i];
+                }
+
+                else
+                    continue;
+                //Nhận 1 số ngẫu nhiên
+                randomNumber = random.Next(0, icons.Count);
+                //icon có thứ tự là số ngẫu nhiên đó sẽ được gán cho label
+                label.Text = icons[randomNumber];
+                //Xóa số ngẫu nhiên khỏi danh sách icon
+                icons.RemoveAt(randomNumber);
+            }
+        }*/
+
+
+
+    }
+}
